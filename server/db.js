2b2a7b1a2f80de06c870480509b15384d5318a22
Gustavo Rodrigues <gustavocr.developer@gmail.com>
2024-10-12 21:59:05 +0100

    suggested changes

--- server/db.js
+++ server/db.js
@@ -1,24 +1,26 @@
 // imports
-const mysql = require('mysql');
-
+import mysql from "mysql";
+// trocar para a lib mysql2
+// https://www.npmjs.com/package/mysql2
 // settings
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    keepAliveInitialDelay: 1000, // Mantém a conexão ativa
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  keepAliveInitialDelay: 1000, // Mantém a conexão ativa
 });
 // run db
-db.connect(err => {
-    if (err) {
-        console.log('Erro ao conectar ao database: ', err);
-    } else {
-        console.log('Servidor conectado com o database')
-    }
+db.connect((err) => {
+  if (err) {
+    console.log("Erro ao conectar ao database: ", err);
+  } else {
+    console.log("Servidor conectado com o database");
+  }
 });
 // tornar o banco acessível nos outros modulos
-module.exports = db;
+export default db;
+// usar export default db; para exportar o banco de dados
--- server/teste.js
+++ server/teste.js
@@ -1,39 +1,39 @@
-const serverUrl = 'http://localhost:3001/bookings';
+// remover esse arquivo se não for usar
+const serverUrl = "http://localhost:3001/bookings";
 
-async function getBookings() {    
-    try {
-        const response = await fetch(serverUrl, {
-            method: 'GET'
-        });
-        
-        const data = await response.json();
-        console.log(data);
+async function getBookings() {
+  try {
+    const response = await fetch(serverUrl, {
+      method: "GET",
+    });
 
-    } catch (error) {
-        console.log(error);
-    }
-};
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 getBookings();
 
-async function createBooking(newBookingData) {    
-    try {
-        const response = await fetch(serverUrl, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newBookingData)
-        });
+async function createBooking(newBookingData) {
+  try {
+    const response = await fetch(serverUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newBookingData),
+    });
 
-        if (response.ok) {
-            getBookings()
-            setAlertMessage('success')
-            setLoading(false); 
-        } else{
-            setAlertMessage('error')
-            setLoading(false);
-        };
-    } catch (error) {
-        setAlertMessage('error')
-        setLoading(false);
-    };
-};
+    if (response.ok) {
+      getBookings();
+      setAlertMessage("success");
+      setLoading(false);
+    } else {
+      setAlertMessage("error");
+      setLoading(false);
+    }
+  } catch (error) {
+    setAlertMessage("error");
+    setLoading(false);
+  }
+}
