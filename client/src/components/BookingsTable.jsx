52e9a62836d92f7a87ca25c2b9a2a2f5a30b97f1
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-10-17 17:41:20 +0100

    #16 Criar uma coluna "serviço" que exibe um sol para almoço ou uma lua para o jantar.

--- client/src/components/BookingsTable.jsx
+++ client/src/components/BookingsTable.jsx
@@ -1,6 +1,23 @@
 import Paper from "@mui/material/Paper";
 import LoadingOverlay from "./LoadingOverlay";
 import { DataGrid } from "@mui/x-data-grid";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import NightlightRoundIcon from '@mui/icons-material/NightlightRound';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+
+const renderedService = (props) => {
+  const serviceProp = props.formattedValue;
+  console.log(serviceProp);
+  if(serviceProp === "almoco") {
+    return <span><WbSunnyIcon /> Almoço</span>; //sx={{color:"black"}}
+
+  } else if (serviceProp === "janta") {
+    return <span><NightlightRoundIcon /> Janta</span>; //color="primary"
+  
+  } else {
+    return <QuestionMarkIcon/>
+  }
+};
 
 const columns = [
   { field: "id", headerName: "ID", width: 20 },
@@ -11,6 +28,7 @@ const columns = [
   { field: "guest_mail", headerName: "Email", width: 200 },
   { field: "booking_status", headerName: "Status", width: 200 },
   { field: "booking_source", headerName: "Source", width: 200 },
+  { field: "service", headerName: "Serviço", width: 100, renderCell: renderedService },
 ];
 
 const paginationModel = { page: 0, pageSize: 50 };
@@ -31,6 +49,7 @@ export default function BookingsTable({
     guest_mail: row.guest_mail,
     booking_status: row.booking_status,
     booking_source: row.booking_source,
+    service: row.service,
   }));
 
   if (loading) {
