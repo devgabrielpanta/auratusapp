71be8c455ae5227c14edc50c3af0711a6abea6f0
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-10-30 12:11:36 +0000

    #53 Firebase instalado no front-end.
    close #53

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_BOOKINGS_ENDPOINT, // colocar nas variáveis de ambientes
+  baseURL: process.env.API_BOOKINGS_ENDPOINT,
 });
 
 export default api;
