747073d60d99e5f42f92f74b6883f470a8719813
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-10 21:21:39 +0000

    #85 authorization default no axios
    close #85

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.API_ENDPOINT,
+  headers: {
+    'authorization': `Bearer ${localStorage.getItem("access_token")}`
+  },
 });
 
 export default api;
