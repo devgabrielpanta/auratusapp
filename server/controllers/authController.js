89e4b3dbdc7d34a3fe39085fcb8617b6b31a1620
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-10 13:14:00 +0000

    #92 códigos do firebase movidos para o authController e serão ajustados nos commits seguintes
    close #92

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,73 @@
 //models
 import * as authModels from "../models/authModels.js";
 //Firebase
-import { signInWithEmail, updateIdToken, verifyToken } from "../middlewares/firebase-api.js";
+import axios from "axios";
+import admin from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
+import serviceAccount from "../firebase-credential.json" assert { type: "json" };
 import bcrypt from "bcrypt";
 import dayjs from "dayjs";
+
+
+//código do firebase-api para limpar e revisar
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+const api_key = process.env.FIREBASE_APIKEY;
+
+// Sign in with email / password: https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+const signInWithEmail = async (email, pass) => {
+    const endpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${api_key}`;
+    try {
+        const response = await axios.post(endpoint, {
+            email: email,
+            password: pass,
+            returnSecureToken: true
+        });
+        const idToken = response.data.idToken;
+        const refreshToken = response.data.refreshToken;
+        const uid = response.data.localId;
+        const setToken = await authModels.setSignInTokens(idToken, refreshToken, uid);
+        return idToken
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+const verifyToken = async (idToken) => {
+    getAuth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+            return decodedToken;
+        })
+        .catch((error) => {
+            throw new Error(error.message);
+        })
+};
+
+// Exchange a refresh token for an ID token: https://firebase.google.com/docs/reference/rest/auth#section-refresh-token
+const updateIdToken = async (refreshToken, next) => {
+    const endpoint = `https://securetoken.googleapis.com/v1/token?key=${api_key}`;
+
+    axios.post(endpoint, {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken
+    })
+    .then((response) => {
+        const idToken = response.data.id_token;
+        const uid = response.data.user_id;
+        authModels.setIdToken(idToken, uid)
+            .then(() => {
+                return idToken;
+            })
+            .catch((error) => {
+                throw new Error(error.message);
+            })
+    })
+    .catch((error) => {
+        throw new Error(error.message);
+    })
+};
 
 export const authValidation = async (req, res) => {
     const email = req.cookies.user;
