3f94f56f533a955e879f6e33263ff22b0349356c
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-14 18:53:33 +0000

    Função que chama o service para gerar um novo token de acesso no servidor antes da expiração do token atual

--- client/src/auth/AuthProvider.jsx
+++ client/src/auth/AuthProvider.jsx
@@ -2,11 +2,24 @@ import {
     createContext,
     useState
 } from "react";
+import { authRefresh } from "../services/auth.js"
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [signedIn, setSignedIn] = useState(localStorage.getItem("access_token") ? true : false);
+
+    const handleRefreshToken = async () => {
+        authRefresh()
+            .then((response) => {
+                localStorage.setItem("access_token", response.token);
+                setSignedIn(true);
+            })
+            .catch((error) => {
+                setSignedIn(false);
+                navigate("/login");
+            })
+    };
     
     const values = {
         signedIn: signedIn,
