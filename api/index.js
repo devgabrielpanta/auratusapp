93737dbbe63e8908e392839671bffd206cb7bc7c
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-18 21:39:58 +0000

    rotas /auth

--- api/index.js
+++ api/index.js
@@ -7,6 +7,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.status(200).json({message: "server live on vercel"})});
 
+app.get("/auth", (req, res) => {
+    res.status(200).json({message: "rota get auth ok"})});
+
+app.post("/auth", (req, res) => {
+    res.status(200).json({message: "rota post auth ok"})});
+
 app.listen(3000, () => console.log("Server running on: http://localhost.com:3000"));
 
 export default app;
