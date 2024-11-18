758a1f5ab6e66a5a508da1cf9c1cf56dc8f367d0
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-18 21:51:26 +0000

    alterado nomenclatura do route e send

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ app.get("/", (req, res) => {
     res.status(200).json({message: "server live on vercel"})});
 
 app.get("/auth", (req, res) => {
-    res.status(200).json({message: "rota get auth ok"})});
+    res.status(200).send({message: "rota get auth ok"})});
 
 app.post("/auth", (req, res) => {
-    res.status(200).json({message: "rota post auth ok"})});
+    res.status(200).send({message: "rota post auth ok"})});
 
 export default app;
