6cae27202ad585c491948d070c63fa1b72d28cec
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-18 21:32:40 +0000

    Vercel get method

--- api/index.js
+++ api/index.js
@@ -5,9 +5,8 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "server live" });
-});
+    res.status(200).json({message: "server live on vercel"})});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server running on: http://localhost.com:3000"));
 
 export default app;
