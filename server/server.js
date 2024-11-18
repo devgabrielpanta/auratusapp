34501cb7c3b2b5a99ca86219be48b1b5ae644567
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-18 14:39:30 +0000

    authLogin direto na raiz do servidor

--- server/server.js
+++ server/server.js
@@ -5,7 +5,14 @@ import bookingsRoutes from "./routes/bookingsRoutes.js";
 import authRoutes from "./routes/authRoutes.js"
 import dotenv from "dotenv";
 import admin from 'firebase-admin';
-
+//authController
+import axios from 'axios';
+import { setLoginTokens, getUserByEmail } from "./models/authModel.js";
+//firebase web
+import { webAuth } from "../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+//firebase admin
+import { getAuth } from 'firebase-admin/auth';
 
 // eslint-disable-next-line no-undef
 dotenv.config();
@@ -24,6 +31,27 @@ admin.initializeApp({
   }),
 });
 
+app.post("/auth/login", async (req, res) => {
+  const email = req.body.email;
+  const pass = req.body.pass;
+  if(!email || !pass) {
+      return res.status(400).send("Email/Senha inválidos");
+  }
+  try {
+      const userCredential = await signInWithEmailAndPassword(webAuth, email, pass);
+      const refresh_token = userCredential._tokenResponse.refreshToken;
+      const uid = userCredential._tokenResponse.localId;
+      const id_token = userCredential._tokenResponse.idToken;
+      const affectedUser = await setLoginTokens(refresh_token, uid);
+      if (affectedUser < 1) {
+          return res.status(502).json({ message: "Usuário não localizado" });
+      } else {
+          return res.status(201).send({token: id_token});
+      }
+  } catch (error) {
+      return res.status(400).json({ message: "Email/Senha inválidos" });
+  }
+});
 app.use("/auth", authRoutes);
 app.use("/bookings", bookingsRoutes);
 
