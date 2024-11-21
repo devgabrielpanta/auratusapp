b743cf345a5d31dc3396627afb43490ca885f140
devgabrielpanta <gabrielpanta.dev@gmail.com>
2024-11-21 12:33:59 +0000

    Permitindo qualquer origem para o options

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,14 +16,14 @@ app.use(express.json());
 
 app.use(cors({
   credentials: true,
-  origin: `${process.env.CLIENT_DOMAIN}/`,
+  origin: process.env.CLIENT_DOMAIN,
   methods: "GET,POST,PUT,OPTIONS",
   allowedHeaders: "authorization,X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version",
 }));
 // cors preflight
 app.options("*", (req, res) => {
   res.set({
-    "Access-Control-Allow-Origin": `"${process.env.CLIENT_DOMAIN}/"`,
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
     "Access-Control-Allow-Headers": "authorization,X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version",
     "Access-Control-Allow-Credentials": "true",
